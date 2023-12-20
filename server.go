522a12edaa79c9ca294b96cdcccb27ea769f6165
Issam Boubcher <99417396+Issaminu@users.noreply.github.com>
2023-12-20 10:53:01 +0100

    spring cleaning

--- server.go
+++ server.go
@@ -5,23 +5,35 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/go-redis/redis"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
+
+	"github.com/go-redis/redis"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	serverPort      = ":3333"
+	redisKey        = "post-"
+	mongoURI        = "mongodb://localhost:27017"
+	redisAddr       = "localhost:6379"
+	redisPassword   = ""
+	redisDB         = "local"
+	redisCollection = "posts"
+	expiration      = 5 * time.Second
 )
 
 func main() {
 	http.HandleFunc("/getPost", getPost)
+	fmt.Printf("Server running on port %s\n", serverPort)
 
-	println("Server running on port 3333")
-	err := http.ListenAndServe(":3333", nil)
+	err := http.ListenAndServe(serverPort, nil)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
 
@@ -30,37 +42,45 @@ func getPost(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 	val, err := getFromRedis(id)
 	var result map[string]interface{}
+
 	if err != nil {
-		// Cache miss from redis
-		println("==== CACHE MISS ON post-" + id + " ====")
+		fmt.Printf("==== CACHE MISS ON %s ====\n", redisKey+id)
 		res, err := getFromMongo(id)
+
 		if errors.Is(err, mongo.ErrNoDocuments) {
-			// DB miss from mongodb
-			println("==== DB MISS ON post-" + id + " ====")
+			fmt.Printf("==== DB MISS ON %s ====\n", redisKey+id)
 			url := "https://jsonplaceholder.typicode.com/posts/" + id
 			response, err := http.Get(url)
-			err = json.NewDecoder(response.Body).Decode(&result)
+
 			if err != nil {
-				fmt.Println("Error fetching data:", err)
+				handleError(w, "Error fetching data", err)
 				return
 			}
+
+			err = json.NewDecoder(response.Body).Decode(&result)
+
+			if err != nil {
+				handleError(w, "Error decoding JSON", err)
+				return
+			}
+
 			_ = saveToMongo(id, result)
 		} else {
-			// DB hit from mongodb
-			println("==== DB HIT ON post-" + id + " ====")
+			fmt.Printf("==== DB HIT ON %s ====\n", redisKey+id)
 			result = res
 		}
 
-		_ = saveToRedis("post-"+id, result)
+		_ = saveToRedis(redisKey+id, result)
 	} else {
-		// Cache hit from redis
-		println("==== CACHE HIT ON post-" + id + " ====")
+		fmt.Printf("==== CACHE HIT ON %s ====\n", redisKey+id)
 		err := json.Unmarshal([]byte(val), &result)
+
 		if err != nil {
-			fmt.Println("Error decoding JSON:", err)
+			handleError(w, "Error decoding JSON", err)
 			return
 		}
 	}
+
 	elapsedTime := time.Since(startTime).Seconds() * 1000
 	fmt.Printf("Execution time: %.5f milliseconds\n", elapsedTime)
 	resultStr, _ := mapToString(result)
@@ -68,121 +88,87 @@ func getPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveToMongo(id string, doc bson.M) error {
-
-	// Set up MongoDB connection options
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		panic(err)
-	}
-
-	// Access the "users" collection
-	collection := client.Database("local").Collection("posts")
+	client, collection := connectToMongo()
+	defer client.Disconnect(context.Background())
 
 	idInt, _ := strconv.Atoi(id)
-
 	filter := bson.D{{"id", idInt}}
-
 	existingDoc := collection.FindOne(context.Background(), filter)
-	// Check if the document exists
+
 	if existingDoc.Err() == nil {
 		fmt.Println("Document already exists.")
 		return fmt.Errorf("document already exists")
 	}
 
-	_, err = collection.InsertOne(context.Background(), doc)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return nil
+	_, err := collection.InsertOne(context.Background(), doc)
+	return err
 }
 
 func getFromMongo(id string) (bson.M, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		panic(err)
-	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		return nil, fmt.Errorf("can't connect to db")
-	}
-
-	collection := client.Database("local").Collection("posts")
+	client, collection := connectToMongo()
+	defer client.Disconnect(context.Background())
 
 	idInt, _ := strconv.Atoi(id)
-
 	filter := bson.D{{"id", idInt}}
 	var result bson.M
-	err = collection.FindOne(context.TODO(), filter).Decode(&result)
-	if err != nil {
-		if errors.Is(err, mongo.ErrNoDocuments) {
-			return nil, err
-		} else {
-			panic(err)
-		}
-	}
+	err := collection.FindOne(context.TODO(), filter).Decode(&result)
 
-	// Close the MongoDB connection
-	defer client.Disconnect(context.TODO())
-
-	return result, nil
+	return result, err
 }
 
 func saveToRedis(key string, val bson.M) error {
 	client := getRedisClient()
-	_, err := client.Ping().Result()
-	if err != nil {
-		fmt.Println("Error connecting to Redis:", err)
-		return fmt.Errorf("can't connect to redis")
-	}
+	defer client.Close()
+
 	value, err := convertBsonMtoJSON(val)
+
 	if err != nil {
 		return fmt.Errorf("can't convert bson to json")
 	}
+
 	strVal, _ := mapToString(value)
-	err = client.Set(key, strVal, 5000000000).Err() // Expiration is in nanoseconds, current value is 5 seconds
-	if err != nil {
-		return fmt.Errorf("can't set key")
-	}
-	return nil
+	err = client.Set(key, strVal, expiration).Err()
+
+	return err
 }
 
 func getFromRedis(postID string) (string, error) {
 	client := getRedisClient()
+	defer client.Close()
 
-	_, err := client.Ping().Result()
-	if err != nil {
-		return "", fmt.Errorf("can't connect to redis")
-	}
+	val, err := client.Get(redisKey + postID).Result()
 
-	val, err := client.Get("post-" + postID).Result()
 	if err == redis.Nil {
 		return "", fmt.Errorf("not found")
 	} else if err != nil {
-		fmt.Println("Error getting value:", err)
-	} else {
+		return "", fmt.Errorf("Error getting value: %v", err)
 	}
-	return val, nil
 
+	return val, nil
 }
 
-func mapToString(inputMap map[string]interface{}) (string, error) {
-	jsonData, err := json.Marshal(inputMap)
+func connectToMongo() (*mongo.Client, *mongo.Collection) {
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
 	if err != nil {
-		return "", err
+		log.Fatal(err)
 	}
 
-	return string(jsonData), nil
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal("Can't connect to the database")
+	}
+
+	collection := client.Database(redisDB).Collection(redisCollection)
+	return client, collection
 }
 
 func getRedisClient() *redis.Client {
 	return redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
+		Addr:     redisAddr,
+		Password: redisPassword,
 		DB:       0,
 	})
 }
@@ -198,16 +184,12 @@ func convertBsonMtoJSON(bsonMap bson.M) (map[string]interface{}, error) {
 	return jsonMap, nil
 }
 
-func printBsonM(bsonMap bson.M) {
-	println("Printing BSON Map:")
-	for key, value := range bsonMap {
-		fmt.Printf("%s: %v\n", key, value)
-	}
+func mapToString(inputMap map[string]interface{}) (string, error) {
+	jsonData, err := json.Marshal(inputMap)
+	return string(jsonData), err
 }
 
-func printMap(inputMap map[string]interface{}) {
-	println("Printing Map:")
-	for key, value := range inputMap {
-		fmt.Printf("%s: %v\n", key, value)
-	}
+func handleError(w http.ResponseWriter, message string, err error) {
+	fmt.Printf("%s: %v\n", message, err)
+	http.Error(w, message, http.StatusInternalServerError)
 }
